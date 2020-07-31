0744dfe83473eff3300d18bed4622938953e0cae
Max Yendall <yendallmax@gmail.com>
2020-07-31 10:37:16 +1000

    Add asset, selling and purchase detail parsing to update inventory response

--- src/Message/InventoryItems/Responses/NewEssentials/UpdateInventoryItemResponse.php
+++ src/Message/InventoryItems/Responses/NewEssentials/UpdateInventoryItemResponse.php
@@ -61,6 +61,87 @@ class UpdateInventoryItemResponse extends AbstractResponse
     }
 
     /**
+     * @param $data
+     * @param $item
+     * @return mixed
+     */
+    public function parsePurchaseDetails($data, $item) {
+        if ($data) {
+            if (array_key_exists('ExpenseAccount', $data)) {
+                if ($data['ExpenseAccount']) {
+                    $item['buying_account_id'] = IndexSanityCheckHelper::indexSanityCheck('UID', $data['ExpenseAccount']);
+                    $item['buying_account_code'] = IndexSanityCheckHelper::indexSanityCheck('DisplayID', $data['ExpenseAccount']);
+                }
+            } else if (array_key_exists('CostOfSalesAccount', $data)) {
+                if ($data['CostOfSalesAccount']) {
+                    $item['buying_account_id'] = IndexSanityCheckHelper::indexSanityCheck('UID', $data['CostOfSalesAccount']);
+                    $item['buying_account_code'] = IndexSanityCheckHelper::indexSanityCheck('DisplayID', $data['CostOfSalesAccount']);
+                }
+            }
+            if (array_key_exists('BuyingDetails', $data)) {
+                if ($data['BuyingDetails']) {
+                    if (array_key_exists('TaxCode', $data['BuyingDetails'])) {
+                        if ($data['BuyingDetails']['TaxCode']) {
+                            $item['buying_tax_type_id'] = IndexSanityCheckHelper::indexSanityCheck('UID', $data['BuyingDetails']['TaxCode']);
+                            $item['buying_tax_type_code'] = IndexSanityCheckHelper::indexSanityCheck('Code', $data['BuyingDetails']['TaxCode']);
+                        }
+                    }
+                    $item['buying_unit_price'] = IndexSanityCheckHelper::indexSanityCheck('StandardCost', $data['BuyingDetails']);
+                }
+            }
+        }
+
+        return $item;
+    }
+
+    /**
+     * @param $data
+     * @param $item
+     * @return mixed
+     */
+    public function parseSellingDetails($data, $item) {
+        if ($data) {
+            if (array_key_exists('IncomeAccount', $data)) {
+                if ($data['IncomeAccount']) {
+                    $item['selling_account_id'] = IndexSanityCheckHelper::indexSanityCheck('UID', $data['IncomeAccount']);
+                    $item['selling_account_code'] = IndexSanityCheckHelper::indexSanityCheck('DisplayID', $data['IncomeAccount']);
+                }
+            }
+            if (array_key_exists('SellingDetails', $data)) {
+                if ($data['SellingDetails']) {
+                    if (array_key_exists('TaxCode', $data['SellingDetails'])) {
+                        if ($data['SellingDetails']['TaxCode']) {
+                            $item['selling_tax_type_id'] = IndexSanityCheckHelper::indexSanityCheck('UID', $data['SellingDetails']['TaxCode']);
+                            $item['selling_tax_type_code'] = IndexSanityCheckHelper::indexSanityCheck('Code', $data['SellingDetails']['TaxCode']);
+                        }
+                    }
+                    $item['selling_unit_price'] = IndexSanityCheckHelper::indexSanityCheck('BaseSellingPrice', $data['SellingDetails']);
+                }
+            }
+        }
+
+        return $item;
+    }
+
+    /**
+     * @param $data
+     * @param $item
+     * @return mixed
+     */
+    private function parseAssetDetails($data, $item) {
+        if ($data) {
+            if (array_key_exists('AssetAccount', $data)) {
+                if ($data['AssetAccount']) {
+                    $item['asset_account_id'] = IndexSanityCheckHelper::indexSanityCheck('UID', $data['AssetAccount']);
+                    $item['asset_account_code'] = IndexSanityCheckHelper::indexSanityCheck('DisplayID', $data['AssetAccount']);
+                }
+            }
+        }
+
+        return $item;
+    }
+
+    /**
      * Return all Contacts with Generic Schema Variable Assignment
      * @return array
      */
