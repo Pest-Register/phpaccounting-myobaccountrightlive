e905ae15c3c81fc4f9b93639d4bfc9829b17a90a
Max Yendall <yendallmax@gmail.com>
2020-08-12 09:57:22 +1000

    Added raw exception to error response helper

--- src/Helpers/NewEssentials/ErrorResponseHelper.php
+++ src/Helpers/NewEssentials/ErrorResponseHelper.php
@@ -15,17 +15,17 @@ class ErrorResponseHelper
         switch ($model) {
             default:
                 if (strpos($response, 'The supplied OAuth token (Bearer) is not valid') !== false) {
-                    $response = [ 'message' => 'The access token has expired' ];
+                    $response = [ 'message' => 'The access token has expired', 'exception' => $response];
                 } elseif (strpos($response, 'already exists') !== false || strpos($response, 'has been taken') !== false || strpos($response, 'DuplicateIdentifier') !== false) {
-                    $response = [ 'message' => 'Duplicate model found' ];
+                    $response = [ 'message' => 'Duplicate model found', 'exception' => $response];
                 } elseif (strpos($response, 'may not be null') !== false) {
-                    $response = [ 'message' => 'Model cannot be edited' ];
+                    $response = [ 'message' => 'Model cannot be edited', 'exception' => $response];
                 } elseif (strpos($response, 'is required') !== false) {
-                    $response = [ 'message' => 'Parameter missing from request: '.$response ];
+                    $response = [ 'message' => 'Parameter missing from request: '.$response, 'exception' => $response];
                 } elseif (strpos($response, 'page not found') !== false ) {
-                    $response = [ 'message' => 'NULL Returned from API or End of Pagination' ];
+                    $response = [ 'message' => 'NULL Returned from API or End of Pagination', 'exception' => $response];
                 } else {
-                    $response = ['message' => $response];
+                    $response = ['message' => $response, 'exception' => $response];
                 }
                 return $response;
         }
