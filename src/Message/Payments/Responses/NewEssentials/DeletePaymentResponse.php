ec57c812f3f74ff14dadcb69d164eab4eaeadac1
max <max@pestregister.com>
2022-08-17 14:26:27 +1000

    BUGFIX: When model doesn't exist, ensure error is handled correctly on all endpoints

--- src/Message/Payments/Responses/NewEssentials/DeletePaymentResponse.php
+++ src/Message/Payments/Responses/NewEssentials/DeletePaymentResponse.php
@@ -158,7 +158,7 @@ class DeletePaymentResponse extends AbstractResponse
      */
     public function getPayments(){
         $payments = [];
-        if (!is_string($this->data)) {
+        if ($this->data && !is_string($this->data)) {
             if (!array_key_exists('Items', $this->data)) {
                 $payment = $this->data;
                 $newPayment = [];
