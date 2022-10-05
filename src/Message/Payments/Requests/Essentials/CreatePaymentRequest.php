18aa6d905c957aa1983a4e1d37ed8feac7aa313b
nicholaszuccarelli <nicholaszuccarelli@gmail.com>
2022-10-05 16:15:15 +1100

    Fix checking index of accounting_id when creating payment

--- src/Message/Payments/Requests/Essentials/CreatePaymentRequest.php
+++ src/Message/Payments/Requests/Essentials/CreatePaymentRequest.php
@@ -225,20 +225,20 @@ class CreatePaymentRequest extends AbstractRequest
 
         $this->issetParam('paymentDate', 'date');
         $this->issetParam('reference', 'reference_id');
-        if ($this->getInvoice() !== null && $this->getAmount()!== null) {
+        if ($this->getInvoice() !== null && $this->getAmount()!== null && isset($this->getInvoice()['accounting_id'])) {
             $this->data['invoices'] = [[
                 "invoice" => ['uid' => $this->getInvoice()['accounting_id']],
                 "paymentAmount" => $this->getAmount()
             ]];
         }
 
-        if ($this->getAccount() !== null) {
+        if ($this->getAccount() !== null && isset($this->getAccount()['accounting_id'])) {
             $this->data['account'] = [
                 'uid' => $this->getAccount()['accounting_id']
             ];
         }
 
-        if ($this->getContact() !== null) {
+        if ($this->getContact() !== null && isset($this->getContact()['accounting_id'])) {
             $this->data['customer'] = [
                 'uid' => $this->getContact()['accounting_id']
             ];
--- src/Message/Payments/Requests/NewEssentials/CreatePaymentRequest.php
+++ src/Message/Payments/Requests/NewEssentials/CreatePaymentRequest.php
@@ -223,7 +223,7 @@ class CreatePaymentRequest extends AbstractRequest
         $this->issetParam('Date', 'date');
         $this->issetParam('Memo', 'reference_id');
         $this->data['DepositTo'] = 'Account';
-        if ($this->getInvoice() !== null && $this->getAmount()!== null) {
+        if ($this->getInvoice() !== null && $this->getAmount()!== null && isset($this->getInvoice()['accounting_id'])) {
             $this->data['Invoices'] = [[
                 "UID" => $this->getInvoice()['accounting_id'],
                 "AmountApplied" => $this->getAmount(),
@@ -231,13 +231,13 @@ class CreatePaymentRequest extends AbstractRequest
             ]];
         }
 
-        if ($this->getAccount() !== null) {
+        if ($this->getAccount() !== null && isset($this->getAccount()['accounting_id'])) {
             $this->data['Account'] = [
                 'UID' => $this->getAccount()['accounting_id']
             ];
         }
 
-        if ($this->getContact() !== null) {
+        if ($this->getContact() !== null && isset($this->getContact()['accounting_id'])) {
             $this->data['Customer'] = [
                 'UID' => $this->getContact()['accounting_id']
             ];
